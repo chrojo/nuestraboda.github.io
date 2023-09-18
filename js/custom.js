f88cc5bba8eac0aced398848a8beb4da7a9ba48a
chrojo <117523099+chrojo@users.noreply.github.com>
2023-09-18 05:00:06 +0200

    Update

--- js/custom.js
+++ js/custom.js
@@ -31,23 +31,27 @@ var x = setInterval(function() {
 
 // Select the div element
 const colorChangeDiv = document.querySelector('.color-change-div');
+const changeBtn = document.querySelector('.show-btn');
 const colorTextChangeDiv = document.querySelectorAll('.nav-link');
 
 // Function to change background color
 function changeBackgroundColorOnScroll() {
     // Check if the page has been scrolled
     if (window.scrollY > 0) {
-        colorChangeDiv.style.backgroundColor = '#C53222'; // Change to the desired color
-        colorTextChangeDiv.forEach(div => {
-          div.style.color = '#FFF8F3'; // Change to the desired color
-        });
+      changeBtn.style.display = 'block';
+      colorChangeDiv.style.backgroundColor = '#C53222'; // Change to the desired color
+      colorTextChangeDiv.forEach(div => {
+        div.style.color = '#FFF8F3'; // Change to the desired color
+      });
     } else {
-        colorChangeDiv.style.backgroundColor = '#fff9f3'; // Reset to the initial color+
-        colorTextChangeDiv.forEach(div => {
-          div.style.color = '#DB9E3C'; // Reset to the initial color
-        });
+      changeBtn.style.display = 'none';
+      colorChangeDiv.style.backgroundColor = '#fff9f3'; // Reset to the initial color+
+      colorTextChangeDiv.forEach(div => {
+        div.style.color = '#DB9E3C'; // Reset to the initial color
+      });
     }
 }
 
 // Add an event listener to the window's scroll event
 window.addEventListener('scroll', changeBackgroundColorOnScroll);
+
